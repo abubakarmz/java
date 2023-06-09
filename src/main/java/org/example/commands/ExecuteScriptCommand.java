c20c29b85ce7e01a4d25d9e052f01a03eee26348
Abubakar <94130190+usernamenameuserusername@users.noreply.github.com>
2023-06-10 00:37:06 +0300

    Update ExecuteScriptCommand.java

--- src/main/java/org/example/commands/ExecuteScriptCommand.java
+++ src/main/java/org/example/commands/ExecuteScriptCommand.java
@@ -1,27 +1,27 @@
-//package org.example.commands;
-//
-//import java.io.BufferedReader;
-//import java.io.FileReader;
-//import java.io.IOException;
-//import org.example.CommandProcessor;
-//import org.example.commands.Command;
-//
-//public class ExecuteScriptCommand extends Command {
-//    private String fileName;
-//
-//    public ExecuteScriptCommand(String fileName) {
-//        this.fileName = fileName;
-//    }
-//
-//    @Override
-//    public void execute() {
-//        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                CommandProcessor.processCommand(line); //
-//            }
-//        } catch (IOException e) {
-//            System.out.println("Ошибка при чтении файла: " + e.getMessage());
-//        }
-//    }
-//}
+package org.example.commands;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import org.example.CommandProcessor;
+import org.example.commands.Command;
+
+public class ExecuteScriptCommand extends Command {
+   private String fileName;
+
+   public ExecuteScriptCommand(String fileName) {
+       this.fileName = fileName;
+   }
+
+   @Override
+   public void execute() {
+       try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+           String line;
+           while ((line = reader.readLine()) != null) {
+               CommandProcessor.processCommand(line); //
+           }
+       } catch (IOException e) {
+           System.out.println("Ошибка при чтении файла: " + e.getMessage());
+       }
+   }
+}
